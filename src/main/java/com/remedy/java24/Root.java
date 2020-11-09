222538240da716bde93d4be358e2188762b71d2f
Joel Amarkwei <joel.amarkwei@gmail.com>
2020-11-09 12:19:37 +0000

    polls

---
+++ src/main/java/com/remedy/java24/Root.java
@@ -0,0 +1,8 @@
+package com.remedy.java24;
+
+public class Root {
+    public static void main(String[] arguments){
+        int number = 144;
+        System.out.println("The root of " + number + " is " + Math.sqrt(number));
+    }
+}
---
+++ src/main/java/com/remedy/java24/Saluton.java
@@ -0,0 +1,11 @@
+package com.remedy.java24;
+
+public class Saluton {
+    public static void main(String[] arguments){
+//        String name = "The Salomondo Program";
+//        System.out.println(name);
+
+//        int number = 225; 
+//        System.out.println("The root of " + number + " is " + Math.sqrt(number));
+    }
+}
