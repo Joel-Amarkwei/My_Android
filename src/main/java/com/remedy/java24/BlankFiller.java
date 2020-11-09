8bc4b68adaad32c82fd38184ab0f56b5d4470641
Joel Amarkwei <joel.amarkwei@gmail.com>
2020-11-09 12:02:51 +0000

    pollar

---
+++ src/main/java/com/remedy/java24/BlankFiller.java
@@ -0,0 +1,8 @@
+
+package com.remedy.java24;
+
+public class BlankFiller {
+       public static void main(String[] arguments){
+           System.out.println("The silly " + arguments[0]  + " jumped over the " + arguments[1] + " and killed the " + arguments[2]);
+       }    
+}
